c6088bdb66623b9a560a5d7a43304b35bcbc5779
Shane Sweeney <shane24sweeney@gmail.com>
2026-01-06 12:50:55 -0500

    removing request mapping for APIs, and putting back mapping and removing try catch commented out

--- src/main/java/com/ecommerce/controller/CategoryController.java
+++ src/main/java/com/ecommerce/controller/CategoryController.java
@@ -5,62 +5,46 @@ import com.ecommerce.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
 
-@Autowired
+    @Autowired
     CategoryService categoryService;
 
 
     @GetMapping("/public/categories")
-    //@RequestMapping(value = "/public/categories", method = RequestMethod.GET )
-    private ResponseEntity<List<Category>> getAllCategories()
-    {
+    private ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories,HttpStatus.OK);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
-    //@RequestMapping(value = "/public/categories", method = RequestMethod.POST )
     @PostMapping("/public/categories")
-    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category)
-    {
+    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully", HttpStatus.CREATED);
     }
 
     @PutMapping("/public/categories/{categoryId}")
-    //@RequestMapping(value = "/public/categories/{categoryId}", method = RequestMethod.PUT )
-    public ResponseEntity<String> updateCategory(@RequestBody Category category,@PathVariable Long categoryId)
-    {
-     //  try {
-           Category savedCategory = categoryService.updateCategory(category, categoryId);
-           return ResponseEntity.status(HttpStatus.OK).body("Category updated successfully");
-           }
-      // }
-//       catch (ResponseStatusException ex)
-//       {return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());}
-//       }
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId) {
+
+        Category savedCategory = categoryService.updateCategory(category, categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body("Category updated successfully");
+    }
+
 
     //No validations or exception handling in controller just a pass through
-       @DeleteMapping("/admin/categories/{categoryId}")
-    //@RequestMapping(value = "/admin/categories/{categoryId}", method = RequestMethod.DELETE )
-    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId)
-    {
-      //  try {
-            String status = categoryService.deleteCategory(categoryId);
-            return ResponseEntity.status(HttpStatus.OK).body(status);
-            // catch(ResponseStatusException ex)
-        }
-//        catch (ResponseStatusException ex)
-//        {return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());}
-//    }
-//
+    @DeleteMapping("/admin/categories/{categoryId}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+
+        String status = categoryService.deleteCategory(categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(status);
+    }
+
 }
