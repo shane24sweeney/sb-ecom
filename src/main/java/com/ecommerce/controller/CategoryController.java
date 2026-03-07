27f6ee8c27437d15aecf30cef3023f1a95cafb80
Shane Sweeney <shane24sweeney@gmail.com>
2026-03-07 08:47:04 -0500

    latest updates to repo

--- src/main/java/com/ecommerce/controller/CategoryController.java
+++ src/main/java/com/ecommerce/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.ecommerce.controller;
 
-import com.ecommerce.model.Category;
 import com.ecommerce.payload.CategoryDTO;
 import com.ecommerce.payload.CategoryResponse;
 import com.ecommerce.service.CategoryService;
@@ -17,15 +16,20 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+//    @GetMapping("/echo")
+//    public ResponseEntity<String> getEcho(@RequestParam(name="message", required = false)  String message)
+//    {
+//        return new ResponseEntity<String>("response is"+message, HttpStatus.OK);
+//    }
+
     @GetMapping("/public/categories")
-    //@RequestMapping(value = "/public/categories", method = RequestMethod.GET)
-    public ResponseEntity<CategoryResponse> getAllCategories(){
-        CategoryResponse categoryResponse = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(@RequestParam(name="pageNumber") Integer pageNumber, @RequestParam(name="pageSize") Integer pageSize){
+
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
     @PostMapping("/public/categories")
-    //@RequestMapping(value = "/public/categories", method = RequestMethod.POST)
     public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
         CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
--- src/main/java/com/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/service/CategoryServiceImpl.java
@@ -8,6 +8,9 @@ import com.ecommerce.payload.CategoryResponse;
 import com.ecommerce.respositories.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,8 +27,11 @@ import java.util.Optional;
     private ModelMapper modelMapper;
 
     @Override
-    public CategoryResponse getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
+
+        List<Category> categories = categoryPage.getContent();
         if (categories.isEmpty())
             throw new APIException("No category created till now.");
 
